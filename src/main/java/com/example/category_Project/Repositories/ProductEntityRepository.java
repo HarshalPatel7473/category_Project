599f34719584dd196d2e10797d1fdd91db100c00
harshalpatel504 <70095696+harshalpatel504@users.noreply.github.com>
2022-04-20 16:47:35 +0530

    search

--- src/main/java/com/example/category_Project/Repositories/ProductEntityRepository.java
+++ src/main/java/com/example/category_Project/Repositories/ProductEntityRepository.java
@@ -2,6 +2,7 @@ package com.example.category_Project.Repositories;
 
 import com.example.category_Project.Entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,13 @@ public interface ProductEntityRepository extends JpaRepository<ProductEntity,Lon
     List<ProductEntity> findAll();
     public ProductEntity findByNameAndCategoryIdAndSubcategoryId(String name
             ,Long categoryId,Long subcategoryId);
-}
+/*    @Query("value = select * from\" +\n" +
+           "     //       \" category_Project.Product where\" +\n" +
+           "       //     \" concat(Product.name,Product.price,Product.weight,Product.GST" +
+            " LIKE %?1%, nativeQuery = true")
+    public List<ProductEntity> search(String name); */
+
+    // @Query("select * from" +
+     //       " category_Project.Product where" +
+       //     " concat(Product.name,Product.price,Product.weight,Product.GST");
+   }
